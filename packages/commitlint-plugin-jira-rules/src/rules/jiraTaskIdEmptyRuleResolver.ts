cbe868cc3f85c992755a14f4b7639a8b5915b364
Jefferson Vieira da Silva <s2it_jvieira@pagseguro.com>
2020-02-06 11:57:08 -0300

    fix: commit message separator in jira-task-id-empty

--- packages/commitlint-plugin-jira-rules/src/rules/jiraTaskIdEmptyRuleResolver.ts
+++ packages/commitlint-plugin-jira-rules/src/rules/jiraTaskIdEmptyRuleResolver.ts
@@ -4,7 +4,11 @@ import {
 } from 'commitlint-jira-utils'
 import { TRuleResolver } from '../../@types'
 
-const jiraTaskIdEmptyRuleResolver: TRuleResolver = parsed => {
+const jiraTaskIdEmptyRuleResolver: TRuleResolver = (
+  parsed,
+  _when,
+  commitMessageSeparator = commitlintJiraConstants.COMMIT_MESSAGE_SEPARATOR
+) => {
   const rawCommitMessage = parsed.raw
   if (!rawCommitMessage) return [false, 'Commit message should not be empty']
 
@@ -13,7 +17,7 @@ const jiraTaskIdEmptyRuleResolver: TRuleResolver = parsed => {
   const isRuleValid = commitMessage.commitTaskIds.length > 0
   return [
     isRuleValid,
-    `the commit message must provide minimum one task id followed by (${commitlintJiraConstants.COMMIT_MESSAGE_SEPARATOR}) symbol, if task not have an id use a conventional task id e.g: "IB-0000${commitlintJiraConstants.COMMIT_MESSAGE_SEPARATOR} My commit message"`,
+    `the commit message must provide minimum one task id followed by (${commitMessageSeparator}) symbol, if task not have an id use a conventional task id e.g: "IB-0000${commitMessageSeparator} My commit message"`,
   ]
 }
 export default jiraTaskIdEmptyRuleResolver
